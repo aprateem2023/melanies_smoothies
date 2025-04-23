c7a40621b7c1217e4ce0433eddd6bd4937b23a42
aprateem2023 <aprateem.roy.2020@gmail.com>
2025-04-23 22:35:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     # st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
@@ -43,5 +45,4 @@ if ingredients_list:
         st.success('Your Smoothie is ordered,'+name_on_order+'!', icon="✅")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
 st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
